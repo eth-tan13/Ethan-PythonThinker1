e28bb4eb14893fa1be25c65f6335eff3973e2455
eth-tan13 <zxethan5@gmail.com>
2026-01-10 07:31:58 +0000

    Lesson 1

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,28 @@
-print("Hello from lesson 3")
+#Task 2
+#variable1 = 3
+#variable2 = 5
+
+#print(variable1 + variable2)
+
+#Task 3
+a = 2
+b = 8
+ans = a + b
+print(ans)
+
+c = 10
+d = 50
+e = 3
+ans = (d - c) * e
+print(ans)
+
+f = 4
+g = 100
+ans = f / g
+print(ans)
+
+h = 3
+i = 5
+j = 10
+ans = (h + f) * i - (j / a)
+print(ans)

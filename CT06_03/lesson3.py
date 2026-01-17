b65cd0630647412a47368901050c374e8643f277
eth-tan13 <zxethan5@gmail.com>
2026-01-17 07:32:45 +0000

    Lesson 2

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,28 +1,67 @@
-#Task 2
-#variable1 = 3
-#variable2 = 5
+# Task 2
+# variable1 = 3
+# variable2 = 5
 
-#print(variable1 + variable2)
+# print(variable1 + variable2)
 
-#Task 3
-a = 2
-b = 8
-ans = a + b
-print(ans)
+# Task 3
 
-c = 10
-d = 50
-e = 3
-ans = (d - c) * e
-print(ans)
+# number = 42.56789
+# number = round(number, 2)
+# print(number)
 
-f = 4
-g = 100
-ans = f / g
-print(ans)
+# number = 1267.15
+# number = round(number)
+# print(number)
 
-h = 3
-i = 5
-j = 10
-ans = (h + f) * i - (j / a)
-print(ans)
+# number = 1785.956
+# number = round(number, -2)
+# print(number)
+
+# Task 4
+# a = 2
+# b = 8
+# ans = a + b
+# print(ans)
+
+# c = 10
+# d = 50
+# e = 3
+# ans = (d - c) * e
+# print(ans)
+
+# f = 4
+# g = 100
+# ans = f / g
+# print(ans)
+
+# h = 3
+# i = 5
+# j = 10
+# ans = (h + f) * i - (j / a)
+# print(ans)
+
+# Task 5
+
+# pineapple_tarts = 15
+# bak_kwa = 58
+# print((pineapple_tarts * 2) + bak_kwa)
+
+# seafood_platter = 96
+# side_dish = 18
+# rice = 2
+# order_total = seafood_platter + (side_dish * 3) + (rice * 6)
+# subtotal = order_total * 1.1 - 25
+# subtotal = round(subtotal, 2)
+# print(subtotal)
+
+# seafood_platter = 100
+# side_dish = 20
+# rice = 2.50
+# order_total = seafood_platter * 5 + side_dish * 8 + rice * 20
+# with_service_charge = order_total * 1.1
+# subtotal = with_service_charge * 1.09
+# subtotal = round(subtotal, 2)
+# print(subtotal)
+
+# 

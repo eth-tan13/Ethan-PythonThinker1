38e29d8a41930e15052aaaca23c7bd1f679b57db
eth-tan13 <zxethan5@gmail.com>
2026-02-07 07:32:03 +0000

    Lesson 5

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,1 +1,46 @@
-print("Hello from lesson 6")
+# print("Hello from lesson 6")
+
+# Task 1
+
+# for i in range(3):
+#     print("Hello, World!")
+
+# for i in range(5):
+#     print(i)
+
+# print("Hello, World!")
+
+# print("Hello, World!")
+
+# Task 2
+
+# age = 10
+# print(age)
+
+# name = "Alice"
+# print(name)
+
+# x = 5
+# print(x)
+
+# print("Hello, World!")
+
+# Task 3
+
+# age = "25"
+# print(int(age) + 1)
+
+# number = 10
+# print(int(number) - 5)
+
+# print("Repeat" * 3)
+
+# year = 2023
+# print("The year is " + str(year))
+
+# x = "10"
+# y = int(x) / 2
+
+# end = "5"
+# for i in range(int(end)):
+#     print(i)
--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,2 +1,32 @@
-print("Hello from lesson 7")
+# print("Hello from lesson 7")
 
+# Recap 1
+
+# score_one = 80
+# score_two = 90
+# score_three = 75
+
+# total = score_one + score_two + score_three
+
+# average_score = total / 3
+
+# student_name = "Alex"
+
+# print("Average score for " + student_name + " is: " + str(average_score))
+
+# Recap 2
+
+# total_marks = 0
+# students = int(input("How many students are there?\n"))
+# for i in range(int(students)):
+#     marks = int(input("Enter marks:\n"))
+#     total_marks = total_marks + marks
+#     average_marks = total_marks / students
+# print("Total = " + str(total_marks))
+# print("Average = " + str(average_marks))
+
+# Recap 3
+
+# name = input("What is your name?\n")
+# age = input("What is your age?\n")
+# print(f"Hello, {name}! You are {age} years old!")

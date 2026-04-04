917cabb285db08637fb19d3439f607ffb43864be
eth-tan13 <zxethan5@gmail.com>
2026-04-04 07:36:58 +0000

    Lesson 10

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,72 @@
-print("Hello from lesson 10")
+# print("Hello from lesson 10")
+
+# Task 1
+
+# num = input("Enter a number!\n")
+# if num > 0:
+#     print(f"{num} is positive.")
+# else:
+#     print(f"{num} is negative.")
+
+# Task 2
+
+# age = int(input("Enter your age!\n"))
+# if age < 13:
+#     print("Child")
+# elif age < 19:
+#     print("Teen")
+# else:
+#     print("Adult")
+
+# Task 3
+
+# temp = int(input("What is the temperature? (degrees celcius)\n"))
+# if temp > 30:
+#     print("Go swimming!")
+# elif temp >= 25:
+#     print("Play basketball!")
+# elif temp >= 20:
+#     print("Go cycling!")
+# else:
+#     print("Read indoors!")
+
+# Task 4
+
+# score = int(input("What is your test score?\n"))
+# if score >= 90:
+#     print("A")
+# elif score >= 80:
+#     print("B")
+# elif score >= 70:
+#     print("C")
+# elif score >= 60:
+#     print("D")
+# else:
+#     print("F")
+
+# Task 5
+
+# while True:
+#     age = int(input("Enter your age!\n"))
+#     if age < 0:
+#         print("Invalid input! Age cannot be negative, please try again!")
+#     elif age < 19:
+#         print("Not eligible to vote.")
+#         break
+#     else:
+#         print("Eligible to vote.")
+#         break
+
+# Task 6
+
+# money = int(input("How much ang pow money did you receive?\n"))
+# if money >= 150:
+#     print("You can buy a gaming keyboard.")
+# elif money >= 100:
+#     print("You can buy a new video game.")
+# elif money >= 50:
+#     print("You can buy a gaming mouse.")
+# elif money >= 20:
+#     print("You can buy a mouse pad.")
+# else:
+#     print("You can buy some snacks.")
--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,1 +1,94 @@
-print("Hello from lesson 11")
+# print("Hello from lesson 11")
+
+# Task 1
+
+# rider1 = int(125)
+# rider2 = int(150)
+# if rider1 > 120 and rider2 > 120:
+#     print("You are eligible to ride!")
+# else:
+#     print("You are not eligible to ride!")
+
+# Task 2
+
+# num = int(input("Input a number!\n"))
+# if num % 7 == 0 and num % 3 == 0:
+#     print(f"{num} is divisible by 3 and 7!")
+# else:
+#     print(f"{num} is not divisible by 3 and 7!")
+
+# Task 3
+
+# first_name = input("What is your first name?\n").lower()
+# last_name = input("What is your last name?\n").lower()
+# if first_name == "james" and last_name == "leong":
+#     print("YOU ARE WANTED.")
+# else:
+#     print("Have a nice day :3")
+
+# Task 4
+
+# rider1 = int(input("Enter your age!\n"))
+# rider2 = int(input("Enter your age!\n"))
+# if rider1 >= 18 or rider2 >= 18:
+#     print("You are allowed to ride!")
+# else:
+#     print("You are not allowed to ride!")
+
+# Task 5
+
+# age = int(input("Enter your age!\n"))
+# if age < 12 or age > 65:
+#     print("Ticket price: $15")
+# else:
+#     print("Ticket price: $20")
+
+# Task 6
+
+# gender = input("Enter your gender:\n").lower()
+# if gender == "m" or gender == "male":
+#     print("Valid input")
+# else:
+#     print("Invalid input")
+
+# Task 7
+
+# while True:
+#     green = input("Enter the correct colour:\n").lower()
+#     if not green == "green":
+#         print("Incorrect colour! Try again.")
+#     else:
+#         print("Correct! The colour is green!")
+#         break
+
+# Task 8
+
+# day = input("What day is it today?\n").lower()
+# if not day == "saturday":
+#     print("It's not the weekend yet!")
+# else:
+#     print("The weekend has arrived. Happy weekend!")
+
+
+# Task 9
+
+# while True:
+#     password = input("Enter your password:\n")
+#     if not password == "Python123":
+#         print("Access Denied.")
+#     else:
+#         print("Correct password. Access Granted.")
+#         break
+
+# Task 11
+
+# username = "John123"
+# password = "pw123"
+# username_input = input("Enter your username:\n")
+# password_input = input("Enter your password:\n")
+# if username_input == username and password_input == password:
+#     print("Access Granted")
+# elif username_input == username or password_input == password:
+#     print("Username or password is incorrect.")
+# else:
+#     print("Access Denied.")

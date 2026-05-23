171cdf545667a2016a7164f27bf16037f92f0bcb
eth-tan13 <zxethan5@gmail.com>
2026-05-23 06:07:13 +0000

    Lesson 15

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -1,17 +1,30 @@
-import random, turtle
-# print("Hello from lesson 14")
+import random
+# print("Hello from lesson 15")
 
-# Recap 1
+# Task 4
 
-# die_roll = []
-# sum = 0
-# for i in range(5):
-#     num = random.randint(1, 6)
-#     die_roll.append(num)
-# print(die_roll)
-# for i in range(len(die_roll)):
-#     sum = sum + die_roll[i-1]
-# print(sum)
-
-# Task 1
-
+moves = ["scissors", "paper", "stone"]
+player_score = 0
+computer_score = 0
+print("--- Welcome to the Scissors Paper Stone Arena! ---")
+while True:
+    if computer_score >= 3 or player_score >= 3:
+        break
+    computer_move = random.choice(moves)
+    player_move = input("Pick (1) scissors, (2) paper, or (3) stone: (enter word)\n").lower()
+    print(f"Computer move: {computer_move}")
+    if player_move not in moves:
+        print("Invalid input! Please try again!")
+    elif (
+        (player_move == "scissors" and computer_move == "paper") or
+        (player_move == "paper" and computer_move == "stone") or
+        (player_move == "stone" and computer_move == "scissors")
+    ):
+        print("You win this round!")
+        player_score += 1
+    else:
+        print("Computer wins this round!")
+        computer_score += 1
+print(f"Final scores:")
+print(f"Player score ({player_score})")
+print(f"Computer score ({computer_score})")
